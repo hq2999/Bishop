1096a5d37161cce94c070c3e2c2c912da29db349
hq <5775335@qq.com>
2017-07-21 15:02:56 +0800

    add readme.txt

--- acp/acp.js
+++ acp/acp.js
@@ -19,7 +19,18 @@ jQuery.fn.extend({
 	        ico_base64 += 'l2BPoxbFmYwWZ0KROtZ5DiR4WQgX/uWQZtaogD3G8AK6q+4xe0sxoAAAAASUVORK5CYII=';
        
 	        _obj.css('background', '#fff url(' + ico_base64 + ') no-repeat ' + (_obj.outerWidth()-15) + 'px '+ (_obj.height()/2-3) +'px');
-				       
+	        
+	        
+	        var item_data = [];
+	        
+	        if(option.offline){
+	        	item_data = option.data;
+	        	for(var i=0;i<item_data.length;i++){
+	        		item_data[i] = item_data[i];
+	        		item_data[i]['index'] = i;
+	        	}
+	        }
+	        
 	        function goPage(page){
 
 	            var url = option.url + "?";
@@ -34,6 +45,13 @@ jQuery.fn.extend({
 			            $("#span_max_page_"+_id).text(data.maxPage);
 			            max_page = data.maxPage;
 			            $("#list_data_" + _id).empty();
+                        
+			            if(option.empty_notice && data.data.length==0){
+			            	show_empty_notice();
+                            return;
+			            }
+			            
+			            
 			            for(var i=0;i<data.data.length;i++){
 			            	var _item_html = option.display(data.data[i]);
 			            	_item_html = _item_html.replace(new RegExp(_obj.val()), "<span class='keyword'>" + _obj.val() + "</span>");
@@ -94,12 +112,51 @@ jQuery.fn.extend({
 	            e.stopPropagation();
 	        }
 	        
+	        function show_offline_item(){
+                $("#empty_notice_" + _id).empty();
+	        	var filter_field = option.filter_field;
+                var empty = true;
+        		for(var i=0;i<item_data.length;i++){
+        			 for(var j=0;j<filter_field.length;j++){
+        				 if(item_data[i][filter_field[j]].match(new RegExp("^" + _obj.val() + ".*"))){
+                            empty = false;
+	        				var _item_html = option.display(item_data[i]);
+	 		            	_item_html = _item_html.replace(new RegExp(_obj.val()), "<span class='keyword'>" + _obj.val() + "</span>");
+	 		            	var _data_html = '<tr><td>' + _item_html + '</td><td style="display:none">' + item_data[i]['index'] + '</td></tr>';
+	 		            	$("#list_data_" + _id).append(_data_html);
+	 		            	
+	 		            	 $("#list_data_" + _id +" tr").on("click", function(){
+	 			                if(option.afterClick){
+	 			                	_obj.val(option.afterClick(item_data[$(this).find('td:eq(1)').text()]));
+	 			                } else {
+	 			                	_obj.val($(this).text());
+	 			                }
+	 			                
+	 			                _acp_list.hide();
+	 			            });
+
+	        			 }
+        			 }
+	            }
+                
+                 if(option.empty_notice && empty){
+                    show_empty_notice();
+                    return;
+                 }
+	        }
 	       
+            function show_empty_notice(){
+                $("#empty_notice_" + _id).empty();
+                $("#empty_notice_" + _id).append(option.empty_notice);
+            }
+           
 	        var _html_panellist = "";
 	        _html_panellist += "<div class='acp_list' id='acp_list_" + _id + "' style='display:none;overflow-y:scroll'>";
 	        _html_panellist += "<table class='list_data' id='list_data_" + _id + "'  cellspacing='0' cellpadding='0'>"
 	        _html_panellist += "</table>"
 	        
+	        _html_panellist += "<div id='empty_notice_" + _id + "'>"
+		    _html_panellist += "</div>"
 	        
 		    _html_panellist += "<div class='list_bar'>";
 	        _html_panellist += "<div class='list_bar_item'><a id='a_first_page_" + _id + "'  href='javascript:void(0)'  title='首页'> 首页 </a></div>";
@@ -133,7 +190,11 @@ jQuery.fn.extend({
 	       
 	        
 	        if(option.autoLoad==true){
-	        	goPage(1);
+	        	if(option.offline){
+	        		show_offline_item();
+	        	} else {
+	        		goPage(1);
+	        	}
 	        }
 	        
 	        _obj.on("focus", function(){
@@ -161,15 +222,17 @@ jQuery.fn.extend({
 	        _obj.on("keyup", function(){
 	        	var e = window.event||arguments[0];
 	        	var code = e.keyCode;
-	        	
-        
-	        	if(delay_flag) {
-	                clearTimeout(delay_flag);
-	            }
-        	    page=1;
-                delay_flag = setTimeout(function(){goPage(1)},option.delay);
-	        	
 
+	        	if(option.offline){
+	        		$("#list_data_" + _id).empty();
+	        		show_offline_item();
+	        	} else {
+	        		if(delay_flag) {
+		                clearTimeout(delay_flag);
+		            }
+	        	    page=1;
+	        	    delay_flag = setTimeout(function(){goPage(1)},option.delay);
+	        	}
 	        });
 
 	        _acp_list.on("mousemove",function(){
---
+++ acp/readme.txt
@@ -0,0 +1,38 @@
+//在线方式，每次keyup都会提交请求到web
+
+$("#cmhc").acp({
+	    url:'<%=basePath%>combox/getVesselVoyage.action',
+	    per_page:1000,                  //每页条数
+	    per_page_str:'perPage',         //每页条数的参数名称（str）
+	    page:1,                         //初始页
+	    height:350,                     //下拉框高度
+	    page_str:'page',                //参数（文本框内容）
+	    param_str:'param',              //参数的名称（str）
+	    param:{},                       //其它参数
+	    autoLoad:true,                  //为true时，点击后直接显示第一页内容，否则keyup时显示
+	    display:function(item){         //下拉框显示的内容
+	    	return item['vesseletrim'] + "/" + item['voyage'];
+	    },
+	    afterClick:function(item){      //显示下拉框后回填到文本框的内容
+	    	return item['vesseletrim'] + "/" + item['voyage'] + "-" + item['vesselcode'];
+	    },
+	    delay:300,                      //击键间隔，单位毫秒，间隔内的keyup将被忽略，避免无意义的提交
+	    empty_notice:"条数为0时，下拉框显示的内容"
+	});	
+    
+//离线方式
+$("#shipNamec").acp({
+    data:data,                         //下拉列表的数据 
+    height:350,                        //下拉框高度
+    offline:true,                      //离线时必须添加
+    filter_field:['label'],            //需要参加过滤的field
+//  autoLoad:true,   
+    display:function(item){            //下拉框显示的内容 
+        return item['label'];
+    },
+    afterClick:function(item){         //显示下拉框后回填到文本框的内容
+        return item['value'];
+    },
+ //   delay:300,
+    empty_notice:"条数为0时，下拉框显示的内容"
+});
